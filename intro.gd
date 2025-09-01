3c6cb661610a9a89ca1b597331091d2dd143f77a
lol72142 <xXMOMMLG360Xx@gmail.com>
2025-09-02 01:57:21 +0700

    testing the cutsence
    test dialog cutsence on intro

--- intro.gd
+++ intro.gd
@@ -1,6 +1,12 @@
 extends Node
 
+@onready var cutse_list = [$Book,$Intro1,$Intro2,$Book]
+var prog = false
+var curent_cutsen
+var cut_dup
+
 func _ready() -> void:
+	SignalBusser.connect("cut_sence",image_change)
 	$Character_Dialog_Layer.visible = false
 	$BlackBackground.visible = true
 	await get_tree().create_timer(1).timeout
@@ -9,3 +15,23 @@ func _ready() -> void:
 		await get_tree().create_timer(0.15).timeout
 	await get_tree().create_timer(1.8).timeout
 	$Character_Dialog_Layer.visible = true
+
+	SignalBusser.emit_signal("display_char_dialog","Intro")
+	
+func image_change():
+	if prog:
+		curent_cutsen.hide()
+		curent_cutsen = cut_dup.pop_front()
+		if cut_dup.size() > 0:
+			curent_cutsen.show()
+			print(cut_dup.size())
+		else:
+			print("in")
+			curent_cutsen.hide()
+			prog = false
+
+	else:
+		cut_dup = cutse_list.duplicate()
+		curent_cutsen = cut_dup.pop_front()
+		curent_cutsen.show()
+		prog = true
