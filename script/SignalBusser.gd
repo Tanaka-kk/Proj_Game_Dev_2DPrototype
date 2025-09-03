49e7021cef728b32385169e440dbe70c691333e7
zD0n <dedmap0526@gmail.com>
2025-09-04 02:49:54 +0700

    saving

--- script/SignalBusser.gd
+++ script/SignalBusser.gd
@@ -5,3 +5,10 @@ signal display_char_dialog(key_j)
 signal answer(contenner)
 signal give_item(item_name)
 signal kill_item()
+<<<<<<< Updated upstream
+=======
+signal cut_sence()
+signal finish_dia()
+signal change_event()
+signal delete_key_dialog()
+>>>>>>> Stashed changes
--- script/dialog_charecter_area.gd
+++ script/dialog_charecter_area.gd
@@ -2,15 +2,28 @@ extends Area2D
 
 @export var dialog_key = ''
 var area_active = false
+<<<<<<< Updated upstream
+=======
+var once_play_bool = true
+var interact = false
+>>>>>>> Stashed changes
 
 func _input(event: InputEvent) -> void:
 	if area_active and event.is_action_pressed("interact"):
 		SignalBusser.emit_signal("display_char_dialog", dialog_key)
-
-
+		interact = true
 
 func _on_area_entered(area: Area2D) -> void:
 	area_active = true
 	
 func _on_area_exited(area: Area2D) -> void:
 	area_active = false
+<<<<<<< Updated upstream
+=======
+
+func _on_instant_area_body_entered(body: Node2D) -> void:
+	if body is Player :
+		SignalBusser.emit_signal("display_char_dialog", dialog_key)
+		area_active = true
+		#SignalBusser.emit_signal("change_event")
+>>>>>>> Stashed changes
