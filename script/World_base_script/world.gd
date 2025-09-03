86a988ff77766b78d6a936f301f107f9487f5d09
rokuryou <143307564+rokuryou@users.noreply.github.com>
2025-09-03 19:54:40 +0700

    update

--- script/World_base_script/world.gd
+++ script/World_base_script/world.gd
@@ -1,10 +1,7 @@
 extends BaseScene
-<<<<<<< HEAD
 	
 func _process(delta: float) -> void:
 	if $Screen_tranfer_Trigger_Area.in_area:
 		$Forniture2/Door.visible = true
 	else:
 		$Forniture2/Door.visible = false
-=======
->>>>>>> a3cc80615cb894642cc08bb7fbf70818983e6a5f
--- script/screen_tranfer_trigger_area.gd
+++ script/screen_tranfer_trigger_area.gd
@@ -5,7 +5,6 @@ class_name Screen_trigger
 @export var connect_scene: String #name sence
 var sence_floder = "res://Scene/"
 var in_area = false
-var in_progress = false
 
 func _on_body_exited(body: Node2D) -> void:
 	if body is Player:
@@ -20,8 +19,7 @@ func _on_body_entered(body: Node2D) -> void:
 		$CollisionShape2D/Press.visible = true
 
 func _input(event: InputEvent) -> void:
-	if in_area and event.is_action_pressed("interact") and !in_progress:
-		in_progress = true
+	if in_area and event.is_action_pressed("interact"):
 		$wrap_scene.visible = true
 		for i in 11:
 			$wrap_scene.modulate.a = 0.1*i
@@ -29,5 +27,4 @@ func _input(event: InputEvent) -> void:
 		await get_tree().create_timer(2).timeout
 		$wrap_scene.visible = false
 		SenceManager.change_screen(get_owner(), connect_scene)
-		in_progress = false
 	
