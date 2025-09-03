10abd3fd91e412a07d82755268a01105fd46ae53
zD0n <dedmap0526@gmail.com>
2025-09-04 06:41:05 +0700

    Update Alpha Version

--- script/dialog_charecter_area.gd
+++ script/dialog_charecter_area.gd
@@ -8,7 +8,7 @@ var interact = false
 
 
 func _input(event: InputEvent) -> void:
-	if area_active and event.is_action_pressed("interact"):
+	if area_active and event.is_action_pressed("interact") and dialog_key != "":
 		SignalBusser.emit_signal("display_char_dialog", dialog_key)
 		interact = true
 
